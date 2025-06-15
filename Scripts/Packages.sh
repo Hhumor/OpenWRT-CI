c90fa65da6260c4621a8a1849218d36e11d23d3a
Hhumor <hhumormail@gmail.com>
2025-06-15 14:02:36 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -67,7 +67,6 @@ UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base q
 UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "cdnspeedtest" "immortalwrt-collections/openwrt-cdnspeedtest" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
